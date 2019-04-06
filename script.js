e39e8b069f1fd0a997ba2a84a830796b4fc8e0be
abdal <abdalamerx@gmail.com>
2019-04-06 15:41:21 -0400

    efuwkil

--- script.js
+++ script.js
@@ -12,16 +12,6 @@ function myFunction() {
   }
 }
 
-function myFunction() {
-  var x = document.getElementById("show");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
-
 
 
 
